38613ab010d0840329f76dbfe13d27fc300bb79f
isabbelafelix <isabbelafelix@gmail.com>
2021-11-16 15:43:28 -0300

    Mudanças na main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,63 @@
+import io.javalin.Javalin
+import io.javalin.apibuilder.ApiBuilder.get
+
 fun main(){
-    println("Olá Deevas!!!")
-    println("Preparadas para construirmos uma API?")
+
+    val userDao = UserDao()
+
+    val app = Javalin.create().apply {
+        exception(Exception::class.java) { e, ctx -> e.printStackTrace()}
+        error(404) { ctx -> ctx.json("NOT FOUND")}
+    }.start(7000)
+
+
+    app.routes {
+        get("/users") { ctx ->
+            ctx.json(userDao.users)
+        }
+
+        get("/users/{user-id}") { ctx ->
+            ctx.json(userDao.findById(ctx.pathParam("user-id").toInt())!!)
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    val app = Javalin.create().start(7000)
+//    app.get("/wcc") { ctx -> ctx.result("Olá Deevas =)")}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    val isabela = User("Isabela", "isabela@wcc.com", 28)
+//
+//    val newNomeIsabela = isabela.copy("Isab")
+//
+//    println(isabela)
+//    println(newNomeIsabela)
 }
+
+
